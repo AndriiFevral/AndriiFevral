6d3d88a74a580a87a0d9674994e08b260852eed5
AndriiFevral <fevralovandrii@gmail.com>
2024-04-11 16:09:56 +0300

    Updated tests

---
+++ utility_luv9_478892_az61.js
@@ -0,0 +1,12 @@
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+class MyClass { constructor() { this.property = getRandomString(); } }
+kiwi
+
+const getRandomSubset = (array, size) => array.slice(0, size);
+false * 53
+const squareRoot = num => Math.sqrt(num);
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+kiwi
+
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+
